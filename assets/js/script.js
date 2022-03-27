4c94c2e061a82c3a7d42901900733be30dc1e16f
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-03-27 18:29:32 -0400

    created global variables and local variables for character length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,30 @@
 // Assignment code here
+var specialCharacters = "!@#$%^&*()_+=-<,>.;':`/?~";
+var numbers = "1234567890";
+var upperCaseCharacters ="ABCEDFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var lengthOfPassword = "";
+// Window to ask how many characters user would like password to be
+
+var generatePassword = function() {
+  var lengthOfPassword = window.prompt("How many characters would you like your password to include?");
+  if(lengthOfPassword < 8 || lengthOfPassword > 128) {
+    alert("Password must be between 8-128 characters!")
+    var lengthOfPassword = window.prompt("How many characters would you like your password to include?")
+  }
+  // Ask user whether or not they would like different characters for password
+    var specialCharactersOk = confirm("Click OK to include special characters in your password.");
+    var numbersOk = confirm("Click OK to include numbers in your password.")
+    var upperCaseCharactersOk = confirm("Click Ok to include upper case letters in your password.");
+    var lowerCaseCharactersOk = confirm("Click OK to include lower case letters in your password.");
+    if (specialCharactersOk == false && numbersOk == false && upperCaseCharactersOk == false && lowerCaseCharactersOk == false) {
+      alert("Password must include at least one of the 4 previous options. Try again!");
+    generatePassword();
+    }
+    else {
+      alert("GENERATING PASSWORD!")
+    }
+  }
 
 
 // Get references to the #generate element
@@ -9,8 +35,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.textContent = password;
 }
 
 // Add event listener to generate button
