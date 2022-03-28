f96669d83cc353e3b21163187faa85daae042b35
browniecharl <99853094+browniecharl@users.noreply.github.com>
2022-03-27 21:52:43 -0400

    finished if statements and randomizing characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-// Assignment code here
+var characters = "";
 var specialCharacters = "!@#$%^&*()_+=-<,>.;':`/?~";
 var numbers = "1234567890";
 var upperCaseCharacters ="ABCEDFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-var lengthOfPassword = "";
+
+
 // Window to ask how many characters user would like password to be
 
 var generatePassword = function() {
   var lengthOfPassword = window.prompt("How many characters would you like your password to include?");
+
   if(lengthOfPassword < 8 || lengthOfPassword > 128) {
     alert("Password must be between 8-128 characters!")
     var lengthOfPassword = window.prompt("How many characters would you like your password to include?")
@@ -17,17 +19,43 @@ var generatePassword = function() {
     var numbersOk = confirm("Click OK to include numbers in your password.")
     var upperCaseCharactersOk = confirm("Click Ok to include upper case letters in your password.");
     var lowerCaseCharactersOk = confirm("Click OK to include lower case letters in your password.");
+
     if (specialCharactersOk == false && numbersOk == false && upperCaseCharactersOk == false && lowerCaseCharactersOk == false) {
-      alert("Password must include at least one of the 4 previous options. Try again!");
-    generatePassword();
+      alert("Password must include at least one of the 4 previous options. Try again!"); 
+      generatePassword();   
+
     }
     else {
       alert("GENERATING PASSWORD!")
+    }  
+      
+    if (specialCharactersOk = true) {
+      characters = characters + specialCharacters
     }
-  }
 
+    if (numbersOk = true) {
+      characters = characters + numbers
+    }
+      
+    if (lowerCaseCharactersOk = true) {
+      characters = characters + lowerCaseCharacters
+    }
+
+    if (upperCaseCharactersOk = true) {
+      characters = characters + upperCaseCharacters
+    }  
+
+    var passwordGenerated = "";
+          for(var i = 0; i < lengthOfPassword; i++) {
+              var value = Math.floor(Math.random() * characters.length);
+              passwordGenerated = passwordGenerated + characters[value];
+            }
+
+            return passwordGenerated;
+          }
 
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
